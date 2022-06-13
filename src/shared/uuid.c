af4915e62ff00cc5bb1e50f1692c5b9c5fc81397
Nikolaj Due Oesterbye <nikolaj@due-oesterbye.dk>
2022-06-13 15:00:56 +0200

    Clean up

--- src/shared/uuid.c
+++ src/shared/uuid.c
@@ -27,24 +27,6 @@
 #include "src/shared/uuid.h"
 #include "src/shared/util.h"
 
-#if 0
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <netinet/in.h>
-
-
-#include "src/shared/bluetooth-definitions.h"
-#include "src/shared/uuid.h"
-#include "src/shared/util.h"
-#endif
-
 static uint128_t bluetooth_base_uuid = {
 	.data = {	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
 			0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB }
@@ -103,11 +85,6 @@ void bt_uuid_to_uuid128(const bt_uuid_t *src, bt_uuid_t *dst)
 	}
 }
 
-/*static int bt_uuid128_cmp(const bt_uuid_t *u1, const bt_uuid_t *u2)
-{
-	return memcmp(&u1->value.u128, &u2->value.u128, sizeof(uint128_t));
-}*/
-
 int bt_uuid16_create(bt_uuid_t *btuuid, uint16_t value)
 {
 	memset(btuuid, 0, sizeof(bt_uuid_t));
@@ -143,161 +120,6 @@ int bt_uuid_cmp(const bt_uuid_t *uuid1, const bt_uuid_t *uuid2)
 
 	return memcmp(&u1.value.u128, &u2.value.u128, sizeof(uint128_t));
 }
-
-#if 0
-/*
- * convert the UUID to string, copying a maximum of n characters.
- */
-int bt_uuid_to_string(const bt_uuid_t *uuid, char *str, size_t n)
-{
-	bt_uuid_t tmp;
-	unsigned int   data0;
-	unsigned short data1;
-	unsigned short data2;
-	unsigned short data3;
-	unsigned int   data4;
-	unsigned short data5;
-	const uint8_t *data;
-
-	if (!uuid || uuid->type == BT_UUID_UNSPEC) {
-		snprintf(str, n, "NULL");
-		return -EINVAL;
-	}
-
-	/* Convert to 128 Bit format */
-	bt_uuid_to_uuid128(uuid, &tmp);
-	data = (uint8_t *) &tmp.value.u128;
-
-	memcpy(&data0, &data[0], 4);
-	memcpy(&data1, &data[4], 2);
-	memcpy(&data2, &data[6], 2);
-	memcpy(&data3, &data[8], 2);
-	memcpy(&data4, &data[10], 4);
-	memcpy(&data5, &data[14], 2);
-
-	snprintf(str, n, "%.8x-%.4x-%.4x-%.4x-%.8x%.4x",
-				ntohl(data0), ntohs(data1),
-				ntohs(data2), ntohs(data3),
-				ntohl(data4), ntohs(data5));
-
-	return 0;
-}
-
-static inline int is_uuid128(const char *string)
-{
-	return (strlen(string) == 36 &&
-			string[8] == '-' &&
-			string[13] == '-' &&
-			string[18] == '-' &&
-			string[23] == '-');
-}
-
-static inline int is_base_uuid128(const char *string)
-{
-	uint16_t uuid;
-	char dummy[2];
-
-	if (!is_uuid128(string))
-		return 0;
-
-	return sscanf(string,
-		"0000%04hx-0000-1000-8000-00805%1[fF]9%1[bB]34%1[fF]%1[bB]",
-		&uuid, dummy, dummy, dummy, dummy) == 5;
-}
-
-static inline int is_uuid32(const char *string)
-{
-	return (strlen(string) == 8 || strlen(string) == 10);
-}
-
-static inline int is_uuid16(const char *string)
-{
-	return (strlen(string) == 4 || strlen(string) == 6);
-}
-
-static int bt_string_to_uuid16(bt_uuid_t *uuid, const char *string)
-{
-	uint16_t u16;
-	char *endptr = NULL;
-
-	u16 = strtol(string, &endptr, 16);
-	if (endptr && (*endptr == '\0' || *endptr == '-')) {
-		bt_uuid16_create(uuid, u16);
-		return 0;
-	}
-
-	return -EINVAL;
-}
-
-static int bt_string_to_uuid32(bt_uuid_t *uuid, const char *string)
-{
-	uint32_t u32;
-	char *endptr = NULL;
-
-	u32 = strtol(string, &endptr, 16);
-	if (endptr && *endptr == '\0') {
-		bt_uuid32_create(uuid, u32);
-		return 0;
-	}
-
-	return -EINVAL;
-}
-
-static int bt_string_to_uuid128(bt_uuid_t *uuid, const char *string)
-{
-	uint32_t data0, data4;
-	uint16_t data1, data2, data3, data5;
-	uint128_t u128;
-	uint8_t *val = (uint8_t *) &u128;
-
-	if (sscanf(string, "%08x-%04hx-%04hx-%04hx-%08x%04hx",
-				&data0, &data1, &data2,
-				&data3, &data4, &data5) != 6)
-		return -EINVAL;
-
-	data0 = htonl(data0);
-	data1 = htons(data1);
-	data2 = htons(data2);
-	data3 = htons(data3);
-	data4 = htonl(data4);
-	data5 = htons(data5);
-
-	memcpy(&val[0], &data0, 4);
-	memcpy(&val[4], &data1, 2);
-	memcpy(&val[6], &data2, 2);
-	memcpy(&val[8], &data3, 2);
-	memcpy(&val[10], &data4, 4);
-	memcpy(&val[14], &data5, 2);
-
-	bt_uuid128_create(uuid, u128);
-
-	return 0;
-}
-
-int bt_string_to_uuid(bt_uuid_t *uuid, const char *string)
-{
-	if (is_base_uuid128(string))
-		return bt_string_to_uuid16(uuid, string + 4);
-	else if (is_uuid128(string))
-		return bt_string_to_uuid128(uuid, string);
-	else if (is_uuid32(string))
-		return bt_string_to_uuid32(uuid, string);
-	else if (is_uuid16(string))
-		return bt_string_to_uuid16(uuid, string);
-
-	return -EINVAL;
-}
-
-int bt_uuid_strcmp(const void *a, const void *b)
-{
-	bt_uuid_t u1, u2;
-
-	bt_string_to_uuid(&u1, a);
-	bt_string_to_uuid(&u2, b);
-
-	return bt_uuid_cmp(&u1, &u2);
-}
-#endif
 
 int bt_uuid_to_le(const bt_uuid_t *src, void *dst)
 {
